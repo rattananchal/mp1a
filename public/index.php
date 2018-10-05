edb06786d90695f7c41117a0b23e434ad238cc38
anchal <akr43@njit.edu>
2018-10-05 12:19:20 -0400

    Fix : Factory Pattern

--- public/index.php
+++ public/index.php
@@ -18,7 +18,12 @@ class main
 class csv {
     public static function getRecords()
     {
-        $rec1 = ' testing the function run';
+        $make = 'Audi';
+        $model = 'A4';
+        $cars = AutomobileFactory::create($make, $model);
+        // print_r($cars);
+        $rec1 [] = $cars;
+        print_r ($rec1);
         return $rec1;
     }
 }
@@ -34,5 +39,32 @@ class system {
     public static function printTable ($table)
     {
         echo $table;
+    }
+}
+
+
+
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
     }
 }
