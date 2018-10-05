135a77ee9212389aa54c3534d4a3dec8433b322b
anchal <akr43@njit.edu>
2018-10-05 11:50:35 -0400

    Fix : Function Run

--- public/index.php
+++ public/index.php
@@ -5,8 +5,34 @@
  * Date: 10/4/18
  * Time: 2:37 PM
  */
-echo 'test143';
-echo 'testing blaa blaa';
-echo 'lets just edit it';
-echo 'hello world';
+ main :: start();
+class main
+{
+    public static function start()
+    {
+        $records = csv :: getRecords();
+        $table = html :: generateTable($records);
+        system :: printTable($table);
+    }
+}
+class csv {
+    public static function getRecords()
+    {
+        $rec1 = ' testing the function run';
+        return $rec1;
+    }
+}
 
+class html {
+    public static function generateTable($record)
+    {
+        $table = $record;
+        return $table;
+    }
+}
+class system {
+    public static function printTable ($table)
+    {
+        echo $table;
+    }
+}
